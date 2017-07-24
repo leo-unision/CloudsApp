b6fe844eb87dbcc31e13445e7ac05c69cbb200af
leochen <unision@gmail.com>
2017-07-24 15:19:42 +0800

    commit ver 1

--- CloudsApp/ViewController.swift
+++ CloudsApp/ViewController.swift
@@ -7,15 +7,71 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://httpbin.org/get").responseJSON
+        {
+            response in
+//            print(response.request as Any)
+//            print(response.response as Any)
+//            print(response.data as Any)
+//            print(response.result)
+            
+//            if let ARRAY = response.result.value
+//            {
+//                print("準備印出 result 中的資料")
+//                print(ARRAY)
+//            }
+            
+            if let JSON = response.result.value
+            {
+                if let dictionary = JSON as? [String:Any]
+                {
+                    if let value = dictionary["origin"] as? String
+                    {
+                        print("解出 key 為 origin 的值")
+                        print(value)
+                    }
+                    
+                    if let value = dictionary["url"] as? String
+                    {
+                        print("解出 key 為 url 的值")
+                        print(value)
+                    }
+                    
+                    if let headers_dictionary = dictionary["headers"] as? [String:Any]
+                    {
+//                        if let value = headers_dictionary["Accept"] as? String
+//                        {
+//                            print("解出 key 為 Accept 的值")
+//                            print(value)
+//                        }
+                        
+                        for (key, value) in headers_dictionary
+                        {
+                            if let value_string = value as? String
+                            {
+                                print(key + ":" + value_string)
+                            }
+                        }
+                        
+                        
+                    }
+                }
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
