9963deae445fdc071ebe63a3ab118ad8dcce5311
leochen <unision@gmail.com>
2017-07-31 15:25:54 +0800

    希望用一行即可處理 JSON 的取得

--- CloudsApp/ViewController.swift
+++ CloudsApp/ViewController.swift
@@ -1,102 +1,20 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController
-{
+class ViewController: UIViewController {
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        Alamofire.request("https://api.github.com/users/octocat/repos").responseJSON
-            { response in
-            
-            //更符合內容的命名變數名稱為 result_value
-            guard let result_value = response.result.value,
-            let array = result_value as? [Any]
-            else
-            {
-                return
-            }
-            
-            for JSON_OBJECT in array
-            {
-                guard let dictionary = JSON_OBJECT as? [String: Any]
-                else
-                {
-                    return
-                }
-                
-                for (key, value) in dictionary
-                {
-                    switch value
-                    {
-                    case is Int:
-                        print("\(key):\(value)(Int)")
-                    case is String:
-                        print("\(key):\(value)(String)")
-                    case is Bool:
-                        print("\(key):\(value)(Bool)")
-                    default:
-                        print("\(key):(unknown type)")
-                    }
-                }
-                
-//                    if let value = dictionary["id"] as? Int {
-//                        print("id: \(value)")
-//                    }
-//                    
-//                    if let value = dictionary["name"] as? String {
-//                        print("name: \(value)")
-//                    }
-//                    
-//                    if let value = dictionary["private"] as? Bool {
-//                        print("private: \(value)")
-//                    }
-//                    
-//                    if let value = dictionary["homepage"] as? String {
-//                        print("homepage: \(value)")
-//                    } else {
-//                        print("homepage: null")//不知道原始格式之下的處理
-//                    }
-                }
-            }
-        
-        Alamofire.request("https://httpbin.org/get").responseJSON
-        { response in
-            
-            guard let JSON_OBJECT = response.result.value,
-                let dictionary = JSON_OBJECT as? [String: Any]
-                else {
-                    return
-            }
-                    
-            guard let origin = dictionary["origin"] as? String else {
-                        return
-            }
-                
-            print("origin: \(origin)")
-                
-            guard let url = dictionary["url"] as? String else {
-                return
-            }
-                
-            print("url: \(url)")
-            
-            let httpbinOrgJson = HttpbinOrgJson(origin: origin, url: url)
-            print(httpbinOrgJson)
-            
-            let httpbinOrgJson2 = HttpbinOrgJson()
-            print(httpbinOrgJson2)
-        }
+        ApiGithubComJson.fetch()
     }
-    
-            
-    override func didReceiveMemoryWarning()
-    {
+        
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 }
-
--- CloudsApp/httpbin.org.json.swift
+++ CloudsApp/httpbin.org.json.swift
@@ -1,24 +1,58 @@
-//
-//  httpbin.org.json.swift
-//  CloudsApp
-//
-//  Created by Leo on 2017/7/31.
-//  Copyright © 2017年 unision. All rights reserved.
-//
-
 import Foundation
 
 //宣告 struct 及使用名稱，定義用來存放 JSON 內容的變數
-struct HttpbinOrgJson
-{
+struct HttpbinOrgJson {
+    
     let origin: String
     let url: String
+    
+    //    init() {//會改寫自動產生的建構子 影響使用
+    //        self.origin = "內部設定初始值"
+    //        self.url = "內部設定初始值"
+    //    }
 }
 
-extension HttpbinOrgJson
-{
-    init() {
+//定義要 throw 的訊息
+enum DecodeJsonError: Error {
+    case missing(String)
+    case invalid(String, Any)
+}
+
+extension HttpbinOrgJson {
+    
+    init() {//寫在這就不會影響
         self.origin = "內部設定初始值"
         self.url = "內部設定初始值"
     }
+    
+    //    init(dictionary: [String: Any]) throws {
+    //        guard let origin = dictionary["origin"] as? String
+    //            else {
+    //                throw DecodeJsonError.missing("origin")
+    //        }
+    //
+    //        guard let url = dictionary["url"] as? String
+    //            else {
+    //                throw DecodeJsonError.missing("url")
+    //        }
+    //
+    //        self.origin = origin
+    //        self.url = url
+    //    }
+    
+    init?(dictionary: [String: Any]) {
+        guard let origin = dictionary["origin"] as? String
+            else {
+                return nil
+        }
+        
+        guard let url = dictionary["url"] as? String
+            else {
+                return nil
+        }
+        
+        self.origin = origin
+        self.url = url
+    }
 }
+
